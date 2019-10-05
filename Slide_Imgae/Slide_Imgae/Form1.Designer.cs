61633e75cc8821b25a83c6930e466b4012f150e4
phucphonhusc <nvpp15598@gmail.com>
2019-10-05 18:43:57 +0700

    hihi

---
+++ Slide_Imgae/Slide_Imgae/Form1.Designer.cs
@@ -0,0 +1,151 @@
+﻿namespace Slide_Imgae
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.btnPrev = new System.Windows.Forms.Button();
+            this.btnOpen = new System.Windows.Forms.Button();
+            this.btnNext = new System.Windows.Forms.Button();
+            this.btnPlay = new System.Windows.Forms.Button();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.White;
+            this.panel1.Controls.Add(this.pictureBox1);
+            this.panel1.Location = new System.Drawing.Point(164, 21);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(612, 343);
+            this.panel1.TabIndex = 0;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.White;
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(612, 343);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // btnPrev
+            // 
+            this.btnPrev.Enabled = false;
+            this.btnPrev.Image = ((System.Drawing.Image)(resources.GetObject("btnPrev.Image")));
+            this.btnPrev.Location = new System.Drawing.Point(318, 375);
+            this.btnPrev.Name = "btnPrev";
+            this.btnPrev.Size = new System.Drawing.Size(78, 80);
+            this.btnPrev.TabIndex = 1;
+            this.btnPrev.UseVisualStyleBackColor = true;
+            this.btnPrev.Click += new System.EventHandler(this.btnPrev_Click);
+            // 
+            // btnOpen
+            // 
+            this.btnOpen.Image = ((System.Drawing.Image)(resources.GetObject("btnOpen.Image")));
+            this.btnOpen.Location = new System.Drawing.Point(26, 375);
+            this.btnOpen.Name = "btnOpen";
+            this.btnOpen.Size = new System.Drawing.Size(77, 70);
+            this.btnOpen.TabIndex = 2;
+            this.btnOpen.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.btnOpen.UseVisualStyleBackColor = true;
+            this.btnOpen.Click += new System.EventHandler(this.btnOpen_Click);
+            // 
+            // btnNext
+            // 
+            this.btnNext.Enabled = false;
+            this.btnNext.Image = ((System.Drawing.Image)(resources.GetObject("btnNext.Image")));
+            this.btnNext.Location = new System.Drawing.Point(588, 375);
+            this.btnNext.Name = "btnNext";
+            this.btnNext.Size = new System.Drawing.Size(75, 80);
+            this.btnNext.TabIndex = 3;
+            this.btnNext.UseVisualStyleBackColor = true;
+            this.btnNext.Click += new System.EventHandler(this.btnNext_Click);
+            // 
+            // btnPlay
+            // 
+            this.btnPlay.Enabled = false;
+            this.btnPlay.Image = ((System.Drawing.Image)(resources.GetObject("btnPlay.Image")));
+            this.btnPlay.ImageAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.btnPlay.Location = new System.Drawing.Point(447, 375);
+            this.btnPlay.Name = "btnPlay";
+            this.btnPlay.Size = new System.Drawing.Size(84, 80);
+            this.btnPlay.TabIndex = 4;
+            this.btnPlay.UseVisualStyleBackColor = true;
+            this.btnPlay.Click += new System.EventHandler(this.btnPlay_Click);
+            // 
+            // folderBrowserDialog1
+            // 
+            this.folderBrowserDialog1.ShowNewFolderButton = false;
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(927, 467);
+            this.Controls.Add(this.btnPlay);
+            this.Controls.Add(this.btnNext);
+            this.Controls.Add(this.btnOpen);
+            this.Controls.Add(this.btnPrev);
+            this.Controls.Add(this.panel1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Slide Image - by PhucPhon";
+            this.panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button btnPrev;
+        private System.Windows.Forms.Button btnOpen;
+        private System.Windows.Forms.Button btnNext;
+        private System.Windows.Forms.Button btnPlay;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
---
+++ Slide_Imgae/Slide_Imgae/Form1.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Slide_Imgae
+{
+    public partial class Form1 : Form
+    {
+        private string[] folderFile = null;
+        private int selected = 0;
+        private int end = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void btnOpen_Click(object sender, EventArgs e)
+        {
+            if (folderBrowserDialog1.ShowDialog() == DialogResult.OK)
+            {
+                string[] part1 = null,  part2 = null;
+
+                part1 = System.IO.Directory.GetFiles(folderBrowserDialog1.SelectedPath, "*.jpg");
+                part2 = System.IO.Directory.GetFiles(folderBrowserDialog1.SelectedPath, "*.png");
+                folderFile = new string[part1.Length + part2.Length];
+
+                Array.Copy(part1, 0, folderFile, 0, part1.Length);
+                Array.Copy(part2, 0, folderFile, part1.Length, part2.Length);
+                selected = 0;
+                end = folderFile.Length;
+                showImage(folderFile[selected]);
+
+                btnPrev.Enabled = true;
+                btnNext.Enabled = true;
+                btnPlay.Enabled = true;
+            }
+        }
+        private void showImage(string path)
+        {
+            Image folderAnh = Image.FromFile(path);
+
+            pictureBox1.Image = folderAnh;
+        }
+        private void prevImage()
+        {
+            if (selected == 0)
+            {
+                selected = folderFile.Length - 1;
+                showImage(folderFile[selected]);
+            }
+            else
+            {
+                selected = selected - 1;
+                showImage(folderFile[selected]);
+            }
+        }
+
+        private void nextImage()
+        {
+            if (selected == folderFile.Length - 1)
+            {
+                selected = 0;
+                showImage(folderFile[selected]);
+            }
+            else
+            {
+                selected = selected + 1;
+                showImage(folderFile[selected]);
+            }
+        }
+        private void btnPrev_Click(object sender, System.EventArgs e)
+        {
+            prevImage();
+        }
+
+        private void btnNext_Click(object sender, System.EventArgs e)
+        {
+            nextImage();
+        }
+
+        private void timer1_Tick(object sender, System.EventArgs e)
+        {
+            nextImage();
+        }
+
+        private void btnPlay_Click(object sender, System.EventArgs e)
+        {
+            if (timer1.Enabled == true)
+            {
+                timer1.Enabled = false;
+                btnPlay.Text = "START";
+                //btnPlay.Image = Image.FromFile()
+            }
+            else
+            {
+                timer1.Enabled = true;
+                btnPlay.Text = "STOP";
+            }
+        }
+
+        private void Form1_Load(object sender, System.EventArgs e)
+        {
+            btnPrev.Enabled = false;
+            btnNext.Enabled = false;
+            btnPlay.Enabled = false;
+        }
+
+    }
+}
